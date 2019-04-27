9bc6d0180710193e650e3f79319966728bd8d97f
rodz3rd2 <rodrigogalura3rd@gmail.com>
2019-04-27 15:31:15 +0800

    use single quote instead of double

--- config/view.php
+++ config/view.php
@@ -3,9 +3,9 @@
 return [
     # error pages path, relative in resources/views path
     'error_pages' => [
-        "500" => "errors/page-500.twig",
-        "405" => "errors/page-405.twig",
-        "404" => "errors/page-404.twig",
-        "403" => "errors/page-403.twig"
+        '500' => "errors/page-500.twig",
+        '405' => "errors/page-405.twig",
+        '404' => "errors/page-404.twig",
+        '403' => "errors/page-403.twig"
     ]
 ];
