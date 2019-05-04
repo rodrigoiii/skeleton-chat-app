1315134b64c83611cb0cc96da506842202ce800f
rodz3rd2 <rodrigogalura3rd@gmail.com>
2019-05-04 10:53:27 +0800

    remove unuseful default value

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host'     => env('DB_HOSTNAME', "localhost"),
-    'port'     => env('DB_PORT', "3306"),
-    'username' => env('DB_USERNAME', "root"),
+    'host'     => env('DB_HOSTNAME'),
+    'port'     => env('DB_PORT'),
+    'username' => env('DB_USERNAME'),
     'password' => env('DB_PASSWORD'),
     'database' => env('DB_NAME'),
 
